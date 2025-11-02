eaab0aea731a581658b54b906417958369329f0b
toqokoDev <toqokoDev@gmail.com>
2025-11-02 22:09:35 +0300

    draft

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiKey: process.env.API_KEY,
-      apiBase: process.env.API_BASE || "http://localhost:8080",
+      apiBase: process.env.API_BASE || "https://botprorok.ru/api",
     },
   },
 });
